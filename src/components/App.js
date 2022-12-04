c22b13cb549d99036f3706279bb985cbe4d46c47
Ankit Rao <110398130+ankierao@users.noreply.github.com>
2022-12-04 15:45:53 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-    <h1 data-ns-test="musicApp">musicApp</h1>
-     <p data-ns-test="musicApp">my first music app</p>
+    <h1 data-ns-test="project-name">musicApp</h1>
+     <p data-ns-test="project-description">my first music app</p>
     </div>
   )
 }
